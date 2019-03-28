1b97a0293d0757e84b2196e166267791dfc09aeb
wuyahuang <wuyahuang@gmail.com>
2019-03-28 17:24:14 +0800

    code formate.

--- index.js
+++ index.js
@@ -18,65 +18,67 @@ function login() {
   var dappName = 'eos-dapp-template';
 
   // First we need to connect to the user's Scatter.
-  ScatterJS.scatter.connect(dappName).then(connected => {
+  ScatterJS.scatter.connect(dappName).then(function () {
 
-    // If the user does not have Scatter or it is Locked or Closed this will return false;
-    if (!connected) {
-      console.log('connect to scatter failed.');
-      return;
-    } else {
-      console.log('connected to scatter.');
-    }
+      // If the user does not have Scatter or it is Locked or Closed this will return false;
+      if (!connected) {
+        console.log('connect to scatter failed.');
+        return;
+      } else {
+        console.log('connected to scatter.');
+      }
 
-    var scatter = ScatterJS.scatter;
+      var scatter = ScatterJS.scatter;
 
-    // Now we need to get an identity from the user.
-    // We're also going to require an account that is connected to the network we're using.
-    var requiredFields = {accounts: [network]};
-    scatter.getIdentity(requiredFields).then(() => {
+      // Now we need to get an identity from the user.
+      // We're also going to require an account that is connected to the network we're using.
+      var requiredFields = {accounts: [network]};
+      scatter.getIdentity(requiredFields).then(function () {
 
-      // Always use the accounts you got back from Scatter. Never hardcode them even if you are prompting
-      // the user for their account name beforehand. They could still give you a different account.
-      // var account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
+        // Always use the accounts you got back from Scatter. Never hardcode them even if you are prompting
+        // the user for their account name beforehand. They could still give you a different account.
+        // var account = scatter.identity.accounts.find(x => x.blockchain === 'eos');
 
-      var account = scatter.identity.accounts[0];
-      console.log(account);
+        var account = scatter.identity.accounts[0];
+        console.log(account);
 
-      // You can pass in any additional options you want into the eosjs reference.
-      var eosOptions = {expireInSeconds: 60};
+        // You can pass in any additional options you want into the eosjs reference.
+        var eosOptions = {expireInSeconds: 60};
 
-      // Get a proxy reference to eosjs which you can use to sign transactions with a user's Scatter.
-      var eos = scatter.eos(network, Eos, eosOptions);
+        // Get a proxy reference to eosjs which you can use to sign transactions with a user's Scatter.
+        var eos = scatter.eos(network, Eos, eosOptions);
 
-      // ----------------------------
-      // Now that we have an identity,
-      // an EOSIO account, and a reference
-      // to an eosjs object we can send a transaction.
-      // ----------------------------
+        // ----------------------------
+        // Now that we have an identity,
+        // an EOSIO account, and a reference
+        // to an eosjs object we can send a transaction.
+        // ----------------------------
 
-      document.getElementById('login').classList.add('hide');
-      document.getElementById('userName').innerHTML = account.name;
+        document.getElementById('login').classList.add('hide');
+        document.getElementById('userName').innerHTML = account.name;
 
-      eos.getAccount(account.name).then(function (result) {
-        renderUserInfo(result);
+        eos.getAccount(account.name).then(function (result) {
+          renderUserInfo(result);
+        });
+
+        // Never assume the account's permission/authority. Always take it from the returned account.
+        var transactionOptions = {authorization: [account.name + '@' + account.authority]};
+
+        // eos.transfer(account.name, 'eosiomeetone', '0.0001 EOS', 'sent from eos-wallet-integrate', transactionOptions).then(function (trx) {
+        //   // That's it!
+        //   console.log('Transaction' + trx);
+        // }).catch(function (error) {
+        //   console.error(error);
+        // });
+
+      }).catch(function (error) {
+        // The user rejected this request, or doesn't have the appropriate requirements.
+        console.log('connect to scatter failed.');
+        console.log(error);
       });
-
-      // Never assume the account's permission/authority. Always take it from the returned account.
-      var transactionOptions = {authorization: [account.name + '@' + account.authority]};
-
-      // eos.transfer(account.name, 'eosiomeetone', '0.0001 EOS', 'sent from eos-wallet-integrate', transactionOptions).then(trx => {
-      //   // That's it!
-      //   console.log('Transaction' + trx);
-      // }).catch(function (error) {
-      //   console.error(error);
-      // });
-
-    }).catch(function (error) {
-      // The user rejected this request, or doesn't have the appropriate requirements.
-      console.log('connect to scatter failed.');
-      console.log(error);
-    });
-  });
+    }
+  )
+  ;
 }
 
 function renderUserInfo(userInfo) {
